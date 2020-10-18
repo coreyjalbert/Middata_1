b0ee85ef1fb9226eb58055092cdcf465dd010e6a
Gary Jalbert <garyjalbert@garys-mbp.middlebury.edu>
2020-10-18 10:23:48 -0400

    Here's a start at how we could do the bar chart for all of the states.  Obviously it could look a bit tidier, but it's a start!

---
+++ All_States_sample.R
@@ -0,0 +1,11 @@
+library(tidyverse)
+library(ggplot2)
+
+gdc <- readRDS(file.choose())
+View(gdc)
+
+gdc %>%
+  filter(year == 2016) %>%
+  ggplot(aes(state, pregnancyrate1519)) + 
+  geom_col()
+  
